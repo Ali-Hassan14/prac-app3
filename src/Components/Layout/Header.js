dc19109511ae32b2a3cd66690a864e81287c25f6
AliHassan1407 <alihassan1407@outlook.com>
2023-02-08 01:34:00 +0500

    Header and Dummy meals data completed

---
+++ src/Components/Layout/Header.js
@@ -0,0 +1,18 @@
+import { Fragment } from 'react';
+import classes from './Header.module.css';
+import mealsImage from '../../Assets/meals.jpg'
+import HeaderCartButton from './HeaderCardButton';
+const Header=(props)=>{
+    return(
+            <Fragment>
+                <header className={classes.header}>
+                    <h1 >React Food App</h1>
+                    <HeaderCartButton/>
+                </header>
+                <div className={classes['main-image']}>
+                    <img src={mealsImage} alt='A table full of delicious Foods...!'/>
+                </div>
+            </Fragment>
+    );
+};
+export default Header;
